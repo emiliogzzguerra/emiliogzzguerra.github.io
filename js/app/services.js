eb9369c2c9aebef85a76ad13d31691f78ea9d3d1
Emilio <emiliogzz24@gmail.com>
2015-12-20 16:08:32 -0600

    Vinik homepage test

--- js/app/services.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.services', [])
-
-    .service('AppService', ['$q', 'ParseConfiguration',
-        function ($q, ParseConfiguration) {
-
-        }]);
--- script.js
+++
@@ -1,7 +0,0 @@
-// Code goes here
-
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.count = 0;
-});
-
